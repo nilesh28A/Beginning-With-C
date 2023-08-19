f85c7b61935ceef80252dde1fd6140d2052d44cc
Nilesh Asodariya <142680610+nilesh28A@users.noreply.github.com>
2023-08-19 21:57:24 +0530

    Add files via upload

---
+++ 0.hello/service.c
@@ -0,0 +1,10 @@
+ # include<stdio.h>
+ // pre processor directive
+
+int main() {
+    printf("Service Team for Multispan IIOT\r");
+    printf("nilesh");
+    return 0;             // all escape sequence in Udamy
+
+    // program structure
+}
---
+++ 0.hello/teams.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    printf("Multispan Sales Team\n");
+    printf("Multispan Service Team\n");
+    printf("Multispan Software Team\n");
+    printf("Multispan Reapairing Team\n");
+    printf("Multispan Hardware Team");
+    return 0;
+
+    //     "\n" for next line.....
+}
