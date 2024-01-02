37939a8679f045c1253fa00d157ff3220e2abea5
Nilesh Patel <142680610+nilesh28A@users.noreply.github.com>
2024-01-02 23:52:34 +0530

    Update formate_specifire.c

--- 1.variable/formate_specifire.c
+++ 1.variable/formate_specifire.c
@@ -4,9 +4,21 @@ int main(){
     int age;
     printf("Enter your age:=");
 
-    /*  %d = integer
+    /*  %c = character
+        %h = short
+        %d = integer
+        %ld = long
+        %lld = long long
+        %u = unsingnd 
         %f = float
-        %c = character   */
+        %lf = double
+        %e = sintific float
+        %le = sintific double
+        %p = pointer
+        %s = string
+        %x = hexadecimal
+        %o = Octal
+    */
 
     scanf("%d", &age);
     printf("Your age is:=%d", age);
